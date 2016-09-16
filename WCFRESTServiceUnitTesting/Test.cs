402dbee08030ba369e732263f84a7d9e353719e1
GuidedByCthulhu <guidedbycthulhu@gmail.com>
2016-09-16 13:42:41 -0400

    Unit Testing a WCF Service Example
    Added a simple example illustrating how to Unit Test the WCF Service.  
    If you change the configuration to run in DEBUG mode, the WebOperationContext will end up mocked.  This allows you to simulate calls to the REST service, without a service host running.

---
+++ WCFRESTServiceUnitTesting/Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using WCFRestService;
+using Moq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using WebOperationContext = System.ServiceModel.Web.MockedWebOperationContext;
+
+namespace WCFRESTServiceUnitTesting
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void ExampleTestCase ()
+		{
+			WCFRESTService restService = new WCFRESTService();
+			Mock<IWebOperationContext> mockContext = new Mock<IWebOperationContext> { DefaultValue = DefaultValue.Mock };
+			using (new MockedWebOperationContext (mockContext.Object)) { 
+				WCFRestService.ExampleObject returnValue = restService.ReturnExample ();
+				Assert.IsNotNull(returnValue);
+				Assert.IsTrue(returnValue.MeaningfulNumber == 42);
+				mockContext.VerifySet(x => x.OutgoingResponse.StatusCode, System.Net.HttpStatusCode.OK);
+			}
+		}
+	}
+}
